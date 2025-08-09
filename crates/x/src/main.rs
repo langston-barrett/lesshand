8e7eff56d7639a780187cf4e63f2cb555a5db78e
Langston Barrett <langston.barrett@gmail.com>
2025-08-09 16:57:34 -0400

    feat(wasm): Add WebAssembly bindings and demo

--- crates/x/src/main.rs
+++ crates/x/src/main.rs
@@ -1,6 +1,7 @@
 mod cli;
 mod empty;
 mod lint;
+mod wasm;
 mod whitespace;
 mod xref;
 
@@ -15,6 +16,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         Command::Lint(conf) => lint::go(conf)?,
         Command::Whitespace(conf) => whitespace::go(conf)?,
         Command::Xref(conf) => xref::go(conf)?,
+        Command::Wasm(conf) => wasm::go(conf)?,
     }
     Ok(())
 }
